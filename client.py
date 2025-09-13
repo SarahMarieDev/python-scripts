54619c742e7da492b96c87ad4168a8f013e8d98a
Sarah Schlueter <sarahmarie73@gmail.com>
2025-09-13 15:38:29 -0500

    UDP socket practice exercise

--- client.py
+++ client.py
@@ -1,14 +1,31 @@
 import socket
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.connect(("0.0.0.0", 1337))
+# # TCP Sockets
 
-print('Connected!')
+# sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# sock.connect(("0.0.0.0", 1337))
+
+# print('Connected!')
+
+# while True:
+#     message = input("[SEND]: ")
+#     sock.send(str.encode(message))
+#     received = sock.recv(1024)
+#     print(received.decode("utf-8"))
+
+# socket.close()
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+print('Ready!')
 
 while True:
     message = input("[SEND]: ")
-    sock.send(str.encode(message))
-    received = sock.recv(1024)
-    print(received.decode("utf-8"))
+    sock.sendto(str.encode(message), ("0.0.0.0", 1337))
+    received = sock.recvfrom(1024)
+    print(received[0].decode("utf-8"))
+    print(received)
 
 socket.close()
+
+
--- server.py
+++ server.py
@@ -1,26 +1,43 @@
 import socket
 
-# Start a socket object using IPv4 properties and TCP protocol
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# Bind to our local computer on IP 0.0.0.0, port 1337
-sock.bind(("0.0.0.0", 1337))
-# Start listening with a backlog size of 10
-sock.listen(10)
+# # TCP Sockets
 
-conn = False
+# # Start a socket object using IPv4 properties and TCP protocol
+# sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# # Bind to our local computer on IP 0.0.0.0, port 1337
+# sock.bind(("0.0.0.0", 1337))
+# # Start listening with a backlog size of 10
+# sock.listen(10)
+
+# conn = False
+
+# while True:
+#     if conn is False:
+#         print('Waiting for connection')
+#         conn, client = sock.accept()
+#         print('Connection from', client)
+#     else:
+#         received = conn.recv(1024)
+#         received_message = received.decode("utf-8")
+#         print("[RECEIEVED]: " + received_message)
+#         conn.send(str.encode("You said: " + received_message))
+#         if received_message.lower() == "exit":
+#             print("Exiting server loop.")
+#             break
+
+# sock.close()
+
+## UDP Sockets
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.bind(("0.0.0.0", 1337))
+
+print('Waiting for incoming data')
 
 while True:
-    if conn is False:
-        print('Waiting for connection')
-        conn, client = sock.accept()
-        print('Connection from', client)
-    else:
-        received = conn.recv(1024)
-        received_message = received.decode("utf-8")
-        print("[RECEIEVED]: " + received_message)
-        conn.send(str.encode("You said: " + received_message))
-        if received_message.lower() == "exit":
-            print("Exiting server loop.")
-            break
+    received, addr = sock.recvfrom(1024)
+    received_message = received.decode("utf-8")
+    print("[RECEIVED]: " + received_message.strip())
+    sock.sendto(str.encode("You said: " + received_message), addr)
 
-sock.close()
+socket.close()
